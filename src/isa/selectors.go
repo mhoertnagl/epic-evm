8483aaadae370ab62a45c4077e9d894f88011c21
Mathias Hörtnagl <mathias.hoertnagl@gmail.com>
2016-06-12 21:48:37 +0200

    spaces.

--- src/isa/selectors.go
+++ src/isa/selectors.go
@@ -3,29 +3,29 @@ package isa
 import "evm/util"
 
 func Op(instr uint32) uint32 {
-	return Bits(instr, 29, 3)
+  return Bits(instr, 29, 3)
 }
 
 func IsImm(instr uint32) bool {
-	return Set(instr, 28)
+  return Set(instr, 28)
 }
 
 func Rz(instr uint32) uint32 {
-	return Bits(instr, 24, 4)
+  return Bits(instr, 24, 4)
 }
 
 func Rx(instr uint32) uint32 {
-	return Bits(instr, 20, 4)
+  return Bits(instr, 20, 4)
 }
 
 func Ry(instr uint32) uint32 {
-	return Bits(instr, 16, 4)
+  return Bits(instr, 16, 4)
 }
 
 func Imm16(instr uint32) uint32 {
-	return Bits(instr, 4, 16)
+  return Bits(instr, 4, 16)
 }
 
 func AluOp(instr uint32) uint32 {
-	return Bits(instr, 0, 4)
+  return Bits(instr, 0, 4)
 }
--- src/util/bits.go
+++ src/util/bits.go
@@ -3,10 +3,10 @@ package util
 // Bits extracts a consecutive portion of length and offest from the LSB of an
 // unsigned integer.
 func Bits(instr uint32, offset uint8, length uint8) uint32 {
-	return (instr >> offset) & ((1 << length) - 1)
+  return (instr >> offset) & ((1 << length) - 1)
 }
 
 // Set returns true if the bit at the specified offset is set.
 func Set(instr uint32, offset uint8) bool {
-	return 1 == (instr & (1 << offset))
+  return 1 == (instr & (1 << offset))
 }
